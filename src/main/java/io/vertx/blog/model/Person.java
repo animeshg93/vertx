741f30a60417fb8401dfbeb8d7db384e588213d3
unknown <animeshg93@utexas.edu>
2018-07-04 12:33:31 -0500

    First commit

---
+++ src/main/java/io/vertx/blog/model/Person.java
@@ -0,0 +1,57 @@
+package io.vertx.blog.model;
+
+public class Person {
+	
+	private String name;
+	private String lastName;
+	private String age;
+	private String weight;
+	private String gender;
+	private String race;
+	private String hero;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getWeight() {
+		return weight;
+	}
+	public void setWeight(String weight) {
+		this.weight = weight;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getRace() {
+		return race;
+	}
+	public void setRace(String race) {
+		this.race = race;
+	}
+	public String getHero() {
+		return hero;
+	}
+	public void setHero(String hero) {
+		this.hero = hero;
+	}
+	
+	
+}
---
+++ src/main/java/io/vertx/blog/model/SuccessResp.java
@@ -0,0 +1,27 @@
+package io.vertx.blog.model;
+
+public class SuccessResp {
+	
+	private String statusCd;
+	private String statusDesc;
+	private String statusDetail;
+	
+	public String getStatusCd() {
+		return statusCd;
+	}
+	public void setStatusCd(String statusCd) {
+		this.statusCd = statusCd;
+	}
+	public String getStatusDesc() {
+		return statusDesc;
+	}
+	public void setStatusDesc(String statusDesc) {
+		this.statusDesc = statusDesc;
+	}
+	public String getStatusDetail() {
+		return statusDetail;
+	}
+	public void setStatusDetail(String statusDetail) {
+		this.statusDetail = statusDetail;
+	}
+}
